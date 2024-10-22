ef44b2e85b5c2f74b81ba4e222582917d2e520bd
Brayden-Smith <bsmith5@ufl.edu>
2024-10-22 19:48:34 -0400

    .

---
+++ API/Controllers/ScheduleController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace Schedule.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ScheduleController : ControllerBase
+    {
+        [HttpGet(Name = "GetSchedule")]
+        public BsonDocument Get()
+        {
+            
+            const string connectionUri = "mongodb+srv://Brayden:nj1NFMrtiYJefpcs@teamscheduler.iww3k.mongodb.net/?retryWrites=true&w=majority&appName=TeamScheduler";
+            var settings = MongoClientSettings.FromConnectionString(connectionUri);
+            settings.ServerApi = new ServerApi(ServerApiVersion.V1);
+            var client = new MongoClient(settings);
+            try {
+                var result = client.GetDatabase("admin").RunCommand<BsonDocument>(new BsonDocument("ping", 1));
+                Console.WriteLine("Pinged your deployment. You successfully connected to MongoDB!");
+                return result;
+            } catch (Exception ex) {
+                Console.WriteLine(ex);
+            }
+
+            return null;
+        }
+    }
+}
